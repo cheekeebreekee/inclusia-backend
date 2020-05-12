b11c13318a4bac3a6ca6657a1a2368603758825a
Anton Filipovich1 <Anton_Filipovich1@epam.com>
2020-05-12 23:17:18 +0300

    add aggregation route, add geocoder, add cache gor geocoder requests

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,15 @@
+async function promisifyArray(arr, func) {
+  return await Promise.all(arr.map((el) => func(el)));
+}
+
 module.exports = {
   async everyAsync(arr, func) {
-    const resultList = await Promise.all(arr.map((el) => func(el)))
+    const resultList = await promisifyArray(arr, func);
 
     return resultList.every((el) => !!el);
+  },
+
+  async mapAsync(arr, func) {
+    return promisifyArray(arr, func);
   }
 }
